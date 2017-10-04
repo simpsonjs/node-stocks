6db562a0778cbcca716f80d4aecd2925e0f49bb1
Joshua Simpson <Joshua@Joshuas-MacBook-Pro.local>
2017-10-03 21:42:55 -0400

    Add CORS package and fix formatting

--- server/models/user.js
+++ server/models/user.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
-var User = mongoose.model('users', {
-    username: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    tickers: {
-      type: Array,
-      required: true,
-      trim: true
-    }
+const User = mongoose.model('users', {
+  username: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  tickers: {
+    type: Array,
+    required: true,
+    trim: true
+  }
 });
 
 module.exports = User;
